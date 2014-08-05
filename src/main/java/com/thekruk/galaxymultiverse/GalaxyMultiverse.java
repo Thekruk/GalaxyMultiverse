dd77782c8017c98fb96b9e9433f5b4664a6f8c27
Thekruk <Benny2003_9@hotmail.com>
2014-08-05 09:59:54 +0200

    Test

---
+++ src/main/java/com/thekruk/galaxymultiverse/GalaxyMultiverse.java
@@ -0,0 +1,38 @@
+package com.thekruk.galaxymultiverse;
+
+import com.thekruk.galaxymultiverse.configuration.ConfigurationHandler;
+import com.thekruk.galaxymultiverse.proxy.IProxy;
+import com.thekruk.galaxymultiverse.reference.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class GalaxyMultiverse
+{
+    @Mod.Instance(Reference.MOD_ID)
+    public static GalaxyMultiverse instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static IProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
