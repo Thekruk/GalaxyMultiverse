0a270a2d533b7348601546be26a8b7e4057db1a9
Thekruk <Benny2003_9@hotmail.com>
2014-08-11 18:50:00 +0200

    More Creative Tab Polishing

--- src/main/java/com/thekruk/galaxymultiverse/creativetab/CreativeTabGAMU.java
+++ src/main/java/com/thekruk/galaxymultiverse/creativetab/CreativeTabGAMU.java
@@ -8,63 +8,39 @@ import net.minecraft.item.Item;
 
 public class CreativeTabGAMU
 {
-    public static final CreativeTabs GAMU_TAB_BLOCKS = new CreativeTabs(Reference.MOD_ID)
+    public static final CreativeTabs GAMU_TAB_BLOCKS = new CreativeTabs(Reference.MOD_ID.toLowerCase() + "Blocks")
     {
         @Override
         public Item getTabIconItem()
         {
             return Item.getItemFromBlock(ModBlocks.triniumore);
         }
-
-        @Override
-        public  String getTranslatedTabLabel()
-        {
-            return "Galaxy Multiverse Blocks";
-        }
     };
 
-    public static final CreativeTabs GAMU_TAB_MACHINE = new CreativeTabs(Reference.MOD_ID)
+    public static final CreativeTabs GAMU_TAB_MACHINE = new CreativeTabs(Reference.MOD_ID.toLowerCase() + "Machines")
     {
         @Override
         public Item getTabIconItem()
         {
             return Item.getItemFromBlock(ModBlocks.triniumore);
         }
-
-        @Override
-        public  String getTranslatedTabLabel()
-        {
-            return "Galaxy Multiverse Machine";
-        }
     };
 
-    public static final CreativeTabs GAMU_TAB_TOOLS = new CreativeTabs(Reference.MOD_ID)
+    public static final CreativeTabs GAMU_TAB_TOOLS = new CreativeTabs(Reference.MOD_ID.toLowerCase() + "Tools")
     {
         @Override
         public Item getTabIconItem()
         {
             return ModItems.trinium;
         }
-
-        @Override
-        public  String getTranslatedTabLabel()
-        {
-            return "Galaxy Multiverse Tools";
-        }
     };
 
-    public static final CreativeTabs GAMU_TAB_MISC = new CreativeTabs(Reference.MOD_ID)
+    public static final CreativeTabs GAMU_TAB_MISC = new CreativeTabs(Reference.MOD_ID.toLowerCase() + "Misc")
     {
         @Override
         public Item getTabIconItem()
         {
             return ModItems.trinium;
-        }
-
-        @Override
-        public  String getTranslatedTabLabel()
-        {
-            return "Galaxy Multiverse Misc";
         }
     };
 }
