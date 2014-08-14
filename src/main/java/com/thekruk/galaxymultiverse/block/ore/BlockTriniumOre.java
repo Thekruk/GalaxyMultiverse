c7556b2fdbd7678aee499e051bdb876f575d8fa2
Thekruk <Benny2003_9@hotmail.com>
2014-08-15 00:21:07 +0200

    Mining level testing.

--- src/main/java/com/thekruk/galaxymultiverse/block/ore/BlockTriniumOre.java
+++ src/main/java/com/thekruk/galaxymultiverse/block/ore/BlockTriniumOre.java
@@ -1,14 +1,18 @@
 package com.thekruk.galaxymultiverse.block.ore;
 
 import com.thekruk.galaxymultiverse.block.BlockGAMU;
+import net.minecraft.block.material.Material;
 
 public class BlockTriniumOre extends BlockGAMU
 {
     public BlockTriniumOre()
     {
-        super();
+        super(Material.rock);
         this.setBlockName("triniumore");
-        this.setLightLevel(15);
+        // this.setLightLevel(15);
+        this.setHardness(6F);
+        this.setResistance(5F);
+        this.setHarvestLevel("pickaxe", 3);
         // this.setStepSound()
         // this.set*   Mange andre ting
     }
